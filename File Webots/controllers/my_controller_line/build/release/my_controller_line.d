408e5a9c6cd69b46dafdeb898b8a450f799c0330
fahmi nanda <fahminandas24@gmail.com>
2023-04-16 04:59:20 +0700

    first commit

---
+++ File Webots/controllers/my_controller_line/build/release/my_controller_line.d
@@ -0,0 +1,5 @@
+build/release/my_controller_line.o: my_controller_line.c \
+ C:\Users\fahmi\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\fahmi\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\fahmi\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
+ C:\Users\fahmi\AppData\Local\Programs\Webots/include/controller/c/webots/distance_sensor.h
